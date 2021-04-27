e0b7fea063a6d0b5d3e6d7c67d5a90aeb0b3c6c5
lysan <lishan@5imalls.cn>
2021-04-27 16:04:15 +0800

    vue

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### 用于记录工作和学习中遇到的问题与解决方法。  ✨
 #### 访问地址：https://wanguni.github.io/
+###### 发布命令：npm run deploy
 
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,65 +1,64 @@
 module.exports = {
-  title: "Universe's Blog",
-  keywords: 'Universe blog',
-  description: "Welcome to my universe",
-  repo: 'https://github.com/WangUni/wangUni.github.io',
-  // base: '/',  // 本地运行需要注释（发布时候使用）
-  theme: 'reco',
+    title: "Universe's Blog",
+    keywords: 'Universe blog',
+    description: "Welcome to my universe",
+    repo: 'https://github.com/WangUni/wangUni.github.io',
+    base: '/', // 本地运行需要注释（发布时候使用）
+    theme: 'reco',
     head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }],
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        
-  ],
-  lastUpdated: 'Last Updated',
-  themeConfig: {
-      logo: '/img/portrait.jpg',
-      type: 'blog',
-      author: 'YuZhou',
-      authorAvatar: '/img/portrait.jpg',
-      modePicker: false,
-    startYear: '2020',
-    subSidebar: 'auto',
-      lastUpdated: 'Last Updated',
-      nav: [
-          { text: '首页', link: '/', icon: 'reco-home' },
-          { text: '时间轴', link: '/timeline/', icon: 'reco-date' }
-      ],
-      blogConfig: {
-        category: {
-          location: 2,     // 在导航栏菜单中所占的位置，默认2
-          text: '分类' // 默认文案 “分类”
+
+    ],
+    lastUpdated: 'Last Updated',
+    themeConfig: {
+        logo: '/img/portrait.jpg',
+        type: 'blog',
+        author: 'YuZhou',
+        authorAvatar: '/img/portrait.jpg',
+        modePicker: false,
+        startYear: '2020',
+        subSidebar: 'auto',
+        lastUpdated: 'Last Updated',
+        nav: [
+            { text: '首页', link: '/', icon: 'reco-home' },
+            { text: '时间轴', link: '/timeline/', icon: 'reco-date' }
+        ],
+        blogConfig: {
+            category: {
+                location: 2, // 在导航栏菜单中所占的位置，默认2
+                text: '分类' // 默认文案 “分类”
+            },
+            tag: {
+                location: 3, // 在导航栏菜单中所占的位置，默认3
+                text: '标签' // 默认文案 “标签”
+            },
+            socialLinks: [ // 信息栏展示社交信息
+                { icon: 'reco-github', link: 'https://github.com/WangUni' }
+            ]
         },
-        tag: {
-          location: 3,     // 在导航栏菜单中所占的位置，默认3
-          text: '标签'      // 默认文案 “标签”
+        valineConfig: {
+            appId: 'dnY20G2hJaf1K4GSyzb5cb5c-gzGzoHsz',
+            appKey: 'FoBlPtlb4Dttee9H480Te4J8',
+            avatar: 'mp',
+            placeholder: 'Please leave a message'
         },
-        socialLinks: [     // 信息栏展示社交信息
-          { icon: 'reco-github', link: 'https://github.com/WangUni' }
+        codeTheme: 'okaidia',
+        friendLink: [{
+                title: 'CRM',
+                desc: 'test',
+                logo: "/img/M.jpg",
+                link: 'https://myhtest.maoye.cn/'
+            },
+            {
+                title: 'maoM',
+                desc: 'test',
+                logo: '/img/M.jpg',
+                link: 'https://makeuptest.maoye.cn/#/login'
+            }
         ]
-      },
-      valineConfig: {
-        appId: 'dnY20G2hJaf1K4GSyzb5cb5c-gzGzoHsz',
-          appKey: 'FoBlPtlb4Dttee9H480Te4J8',
-          avatar: 'mp',
-          placeholder: 'Please leave a message'
-      },
-      codeTheme: 'okaidia',
-      friendLink: [
-        {
-          title: 'CRM',
-          desc: 'test',
-          logo: "/img/M.jpg",
-          link: 'https://myhtest.maoye.cn/'
-        },
-        {
-          title: 'maoM',
-          desc: 'test',
-          logo: '/img/M.jpg',
-          link: 'https://makeuptest.maoye.cn/#/login'
-        }
-      ]
     },
     markdown: {
-      lineNumbers: true
+        lineNumbers: true
     }
 }
